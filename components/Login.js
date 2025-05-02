98b8115fbff0f0cab4f6cba89922a0904b7146ec
Your nameMarion <marion.rdx@gmail.com>
2025-05-02 16:39:01 +0200

    css images first page ok

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/login.module.css";
 import { useState } from "react";
 import { Modal } from "antd";
 import { login } from "../reducers/user";
